36d427dfb9191e5e3e90180fac8415d51375a410
szmisztal <sz.misztal@gmail.com>
2024-09-02 13:45:13 +0200

    move Sript method to Program class, delete SortingScript class

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -5,7 +5,12 @@
         public static void Main(string[] args)
         {
             List<int> numbers = new List<int>() { 6, 5, 3, 1, 8, 7, 2, 4 };
-            Console.WriteLine(SortingScript.Sort(numbers));
+            Console.WriteLine(Sort(numbers));
+        }
+
+        public static List<int> Sort(List<int> numbers)
+        {
+            return numbers;
         }
     }
 }
