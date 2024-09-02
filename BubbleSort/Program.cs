48b26587b7fc043299790d0d40e61d110a4d0c65
szmisztal <sz.misztal@gmail.com>
2024-09-02 18:47:29 +0200

    add my implementation of bubble sorting

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -4,13 +4,26 @@
     {
         public static void Main(string[] args)
         {
-            List<int> numbers = new List<int>() { 6, 5, 3, 1, 8, 7, 2, 4 };
-            Console.WriteLine(Sort(numbers));
+            List<int> numbers = new() { 6, 5, 3, 1, 8, 7, 2, 4 };
+            Sort(numbers);
         }
 
-        public static List<int> Sort(List<int> numbers)
+        public static void Sort(List<int> numbers)
         {
-            return numbers;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int j = 0; j < numbers.Count - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1]) { SwapNumbers(numbers, j, j + 1); };
+                }
+            }
+        }
+
+        public static void SwapNumbers(List<int> numbers, int firstIndex, int secondIndex)
+        {
+            int temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
         }
     }
 }
